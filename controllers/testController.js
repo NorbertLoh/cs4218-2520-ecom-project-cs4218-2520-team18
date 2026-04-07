13c08063b017e6af2d7572521396869b504cd71e
SherwynNg <sherwyn2000@hotmail.com>
2026-04-08 00:30:58 +0800

    create dummy function to trigger coderabbit auto generate unit test feature

--- controllers/testController.js
+++ controllers/testController.js
@@ -2,18 +2,22 @@
 
 //test controller
 
+export const createDummyBackendFunction = () => {
+  return 'dummy backend function';
+};
+
 export const testController = (req, res) => {
-    try {
-        return res.status(200).send({
-            success: true,
-            message: "Protected route accessed successfully",
-        });
-    } catch (error) {
-        console.error(error);
-        return res.status(500).send({
-            success: false,
-            message: "Error in Test",
-            error,
-        });
-    }
+  try {
+    return res.status(200).send({
+      success: true,
+      message: 'Protected route accessed successfully',
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send({
+      success: false,
+      message: 'Error in Test',
+      error,
+    });
+  }
 };
